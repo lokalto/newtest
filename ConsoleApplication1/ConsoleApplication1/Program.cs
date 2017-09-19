4db52109e313df645a4aedb8c1f3319f394bc713
lokalto <lokaltotest1@gmail.com>
2017-09-19 14:23:29 +0530

    First

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("sad sad");
+            Console.WriteLine("Rahul");
         }
     }
 }
