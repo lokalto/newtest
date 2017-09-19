f7528e0c8a812395647243932254486af0a1fff7
DrAjayKashyap <kajay3320@gmail.com>
2017-09-19 14:08:37 +0530

    ha ha

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Rahul Lvde ka ball");
+            Console.WriteLine("Rahul");
         }
     }
 }
